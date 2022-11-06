8f05ab968690d3fea2468f589838cac2a1914b29
Luckinfort8 <krodriguezt2002@alumno.ipn.mx>
2022-11-06 16:59:25 -0600

    commit algo de javascript

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 jQuery('document').ready(function($){
 
-  var menuBoton = $('.barras-menu'), 
+  var menuBoton = $('.barras-menu'),
       menu = $('.navigation ul');
-  
+
   menuBoton.click(function() {
 
     if (menu.hasClass ('show')) {
@@ -10,7 +10,51 @@ jQuery('document').ready(function($){
     } else {
       menu.addClass('show');
     }
-    
+
   });
 
 });
+
+const sectionPrincipal = document.getElementById('section-content');
+const br = document.createElement('br');
+let a = document.createElement('a');
+a.textContent = ' Haz click aquí';
+
+let posicion = 1;
+
+function cambio(posicion) {
+
+  const p = sectionPrincipal.querySelector('p');
+
+  let p1 = document.createTextNode(P1_LIST[posicion]);
+    let p2 = document.createTextNode(P2_LIST[posicion]);
+
+    sectionPrincipal.querySelector('h2').textContent = H_LIST[posicion];
+    sectionPrincipal.querySelector('img').src = PHOTO_LIST[posicion];
+
+    a.setAttribute('href', ENLACES_LIST[posicion]);
+
+    p.textContent = '';
+    p.appendChild(p1);
+    p.appendChild(br);
+    p.appendChild(p2);
+    p.appendChild(a);
+}
+
+function siguiente(event) {
+  const boton = event.currentTarget;
+
+  if (boton.getAttribute('value') === 'siguiente' && posicion < 3) {
+    posicion++;
+
+    cambio(posicion);
+  }
+  if (boton.getAttribute('value') === 'anterior' && posicion > 0){
+    posicion--;
+
+    cambio(posicion);
+  }
+}
+
+const botonSiguiente = document.getElementById('siguiente').addEventListener('click', siguiente);
+const botonAnterior = document.getElementById('anterior').addEventListener('click', siguiente);
---
+++ scripts/constantes.js
@@ -0,0 +1,33 @@
+const H_LIST = [
+  'Hamburguesas Burguety',
+  '¡Conoce nuestra historia!',
+  '¡Contactanos!',
+  'Sabias que...'
+];
+
+const P1_LIST = [
+  '¿Estas buscando una hamburguesa para saciar tu hambre?',
+  'En burguety nos preocupamos por la salud de nuestros clientes y su satisfacción.',
+  '¿Te gustaría ponerte en contacto con nosotros?',
+  'Una hamburguesa promedio tiene unas 295 calorias; además, si no comiste bien, una buena hamburguesa de burguety puede aportarte eso que te falto en el día.'
+];
+
+const P2_LIST = [
+  '¡Estas en el lugar correcto!', 
+  'Conoce como burguety surgió, nuestras caídas y como logramos llegar a muchas partes, para conocer nuestra historia',
+  '',''
+];
+
+const PHOTO_LIST = [
+  'images/hamb-inicio.jpg',
+  'images/image.jpg',
+  'images/contacto.png',
+  'images/pikachu.jpg'
+];
+
+const ENLACES_LIST = [
+  'vistas/menu.html',
+  'vistas/nosotros.html',
+  'vistas/contactanos.html',
+  'https://www.eluniversal.com.mx/menu/cuantas-calorias-tiene-una-hamburguesa'
+]
