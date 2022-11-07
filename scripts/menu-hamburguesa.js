1919ca71fe8ed94f120001c171a1a0eaa5e0b19f
Luckinfort8 <krodriguezt2002@alumno.ipn.mx>
2022-11-06 22:56:39 -0600

    commit agregadoLinks con Javascript

--- scripts/menu-hamburguesa.js
+++ scripts/menu-hamburguesa.js
@@ -1,3 +1,28 @@
+const RELS_LIST = [
+  'apple-touch-icon-144x144',
+  'apple-touch-icon-72x72',
+  'apple-touch-icon-precomposed',
+  'shortcut icon'
+]
+
+const HREFS_LIST = [
+  '../images/icons/apple-icon-114x114.png',
+  '../images/icons/apple-icon-57x57.png',
+  '../images/icons/apple-icon-60x60.png',
+  '../images/icons/favicon.ico'
+]
+
+const head = document.querySelector('head');
+const final = document.querySelector('#final');
+for (let i = 0; i < 4; i++) {
+
+  let link = document.createElement('link');
+  link.setAttribute('rel',RELS_LIST[i]);
+  link.setAttribute('href',HREFS_LIST[i]);
+
+  head.insertBefore(link,final)
+}
+
 const navegacion = document.getElementById('navegacion');
 
 function ocultar() {
